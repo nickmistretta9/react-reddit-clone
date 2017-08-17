4773f170b2701f4482af3e312e123f8a3d52f4e2
Nick Mistretta <nickmistretta9@gmail.com>
2017-08-17 08:43:58 -0400

    Initial commit

---
+++ src/containers/App/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+	apiKey: "AIzaSyAlySRSkhGbQ-c1cQa5gnxfhhDSny6g2ZQ",
+    authDomain: "reddit-clone-7c4cd.firebaseapp.com",
+    databaseURL: "https://reddit-clone-7c4cd.firebaseio.com",
+    projectId: "reddit-clone-7c4cd",
+    storageBucket: "",
+    messagingSenderId: "1081019124621"
+};
